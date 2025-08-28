87ce8b618248d8b7ede58de501a6c537a26dda89
div468 <julianandre.divas@gmail.com>
2025-08-28 16:19:59 -0600

    feat add: TeamMember Data Class

---
+++ app/src/main/java/com/uvg/teamapp/model/TeamMember.kt
@@ -0,0 +1,19 @@
+package com.uvg.teamapp.model
+/*
+Universidad del Valle de Guatemala
+Programación de Plataformas Móviles
+Julián Divas - 24687
+ */
+data class TeamMember (
+    val name: String,
+    val description: String
+) {
+
+    fun getName(): String{
+        return name
+    }
+
+    fun getDescription(): String{
+        return description
+    }
+}
